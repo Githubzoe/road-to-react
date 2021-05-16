22db424856d2ac50175d3e6968faa334682c0d55
Ye Shi <zoe.shiye@gmail.com>
2021-05-17 04:42:41 +1000

    added search/filter

--- src/App.js
+++ src/App.js
@@ -1,43 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+import React, {useState} from 'react';
 
 function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  }
+
+  const searchedStories = stories.filter(story => 
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search onSearch={handleSearch} />
 
       <hr />
 
-      <List />
+      <List list={searchedStories} />
     </div>
   );
 }
 
-const List = () =>
-  list.map((item) => (
+const Search = props => {
+
+    return (
+      <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={props.onSearch} />
+      </div>
+    );
+}
+
+const List = props =>
+  props.list.map((item) => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -47,5 +66,5 @@ const List = () =>
       <span>{item.points}</span>
     </div>
   ));
-  
+
 export default App;
